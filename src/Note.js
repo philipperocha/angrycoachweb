0b1c884b7ff71e9cb90c57a4f4afca9c29c8b19c
Philippe <philippe2008@hotmail.com>
2018-02-11 13:12:27 -0800

    Add finish task

--- src/Note.js
+++ src/Note.js
@@ -4,19 +4,48 @@ import {Button, ListGroup, ListGroupItem, Grid, Row, Col, Navbar,
     FormGroup, InputGroup, FormControl, DropdownButton, MenuItem} from 'react-bootstrap/lib';
 
 class Note extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+          done: false,
+          textButtonDone: 'Done',
+        }
+
+        this.done = this.done.bind(this); //Porque tive que botar isso pra funcionar o State??
+    }
+
+    done(){
+        if (this.state.done){
+            this.setState({ done: false, textButtonDone: 'Done' });
+        }else{
+            this.setState({ done: true, textButtonDone: 'Cancel' });
+        }
+        console.log(this.state.done);
+    }
+
     render(){
+
+        let text;
+
+        if (this.state.done){
+            text = <div className='text-line-through'>{this.props.text}</div>
+        }else{
+            text = <div className='pull-left'>{this.props.text}</div>
+        }
+
         return(
             <Row className="show-grid">
                 <Col xs={12} md={2}/>
                 <Col xs={12} md={8}>
                     <div className="note">
                         <ListGroupItem>
-                            <div className="pull-left">
-                                {this.props.text}
+                            <div>
+                               {text}
                             </div>
                             <div className="pull-right">
-                                <Button bsStyle="success">
-                                    Done
+                                <Button bsStyle="success" onClick={this.done}>
+                                    {this.state.textButtonDone}
                                 </Button>
                                 <Button bsStyle="danger" onClick={this.props.delete}>
                                     Delete
